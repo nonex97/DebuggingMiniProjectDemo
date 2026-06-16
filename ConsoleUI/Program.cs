06757ae1cf4d86e5c48e3962105e7e94787287e6
nonex97 <ngrguric97@gmail.com>
2026-06-16 20:29:12 +0200

    Correct starting point

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            double i = W(0);
+            int i = W(0);
             Console.WriteLine(i);
             Console.ReadLine();
         }
