1c41ebb936c88a0eee4a286512c21be9cb43c54a
nonex97 <ngrguric97@gmail.com>
2026-06-16 20:16:14 +0200

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i = W(0);
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+
+        private static int W(int e)
+        {
+            int n = 1;
+
+            n = J(e) + 4;
+
+            return n;
+        }
+
+        private static int P(int v)
+        {
+            int o = 0;
+
+            o = F(v) + 2;
+
+            return o;
+        }
+
+        private static int F(int x)
+        {
+            int y = 0;
+
+            y = x / 5;
+
+            return y;
+        }
+
+        private static int J(int a)
+        {
+            int c = 0;
+
+            a = a - 1;
+
+            c = R(a);
+
+            c = P(c) / 3;
+
+            return c;
+        }
+
+        private static int R(int v)
+        {
+            int z = 5;
+
+            if (v == 0)
+            {
+                throw new ArgumentException("We cannot divide by zero", "v");
+            }
+            else
+            {
+                z = 3 / v;
+            }
+
+            return z;
+        }
+    }
+}
